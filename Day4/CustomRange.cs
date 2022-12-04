ebf078b351a2624612f0b0f4335be57dc3d38d71
Jürgen Sunkler <juergen.sunkler@gmail.com>
2022-12-04 23:27:17 +0100

    Day 4 completed

---
+++ Day4/CustomRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class CustomRange
+    {
+
+        public int LowerIndex { get; set; }
+        public int UpperIndex { get; set; }
+
+        public bool Includes(CustomRange otherRange)
+        {
+            if (otherRange.LowerIndex >= LowerIndex && otherRange.UpperIndex <= UpperIndex) return true;
+            return false;
+        }
+
+        public CustomRange()
+        {
+
+        }
+
+        public CustomRange(int lowerIndex, int upperIndex)
+        {
+            LowerIndex = lowerIndex;
+            UpperIndex = upperIndex;
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,52 @@
+﻿using Day4;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+string assemblyPath = Assembly.GetExecutingAssembly().Location;
+string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+string textPath = Path.Combine(assemblyDirectory, "input.txt");
+
+string[] input = File.ReadAllLines(textPath);
+
+// Part 1
+
+int counter = 0;
+
+foreach (string line in input)
+{
+    var match = Regex.Match(line, @"^(\d+)-(\d+),(\d+)-(\d+)$");
+    int firstLower = int.Parse(match.Groups[1].Value);
+    int firstUpper = int.Parse(match.Groups[2].Value);
+    int secondLower = int.Parse(match.Groups[3].Value);
+    int secondUpper = int.Parse(match.Groups[4].Value);
+
+    CustomRange r1 = new CustomRange(firstLower, firstUpper);
+    CustomRange r2 = new CustomRange(secondLower, secondUpper);
+
+    if (r1.Includes(r2) || r2.Includes(r1)) counter++;
+}
+
+Console.WriteLine($"Teil 1: {counter}");
+
+// Part 2
+
+counter = 0;
+
+foreach (string line in input)
+{
+    var match = Regex.Match(line, @"^(\d+)-(\d+),(\d+)-(\d+)$");
+    int firstLower = int.Parse(match.Groups[1].Value);
+    int firstUpper = int.Parse(match.Groups[2].Value);
+    int secondLower = int.Parse(match.Groups[3].Value);
+    int secondUpper = int.Parse(match.Groups[4].Value);
+
+    List<int> first = new List<int>();
+    List<int> second = new List<int>();
+
+    first.AddRange(Enumerable.Range(firstLower, firstUpper-firstLower+1));
+    second.AddRange(Enumerable.Range(secondLower, secondUpper-secondLower+1));
+
+    if (first.Intersect(second).Any()) counter++;
+}
+
+Console.WriteLine($"Teil 2: {counter}");
