03a812a5ce52f8b8786434a200cc16cb18b3b8eb
Jürgen Sunkler <juergen.sunkler@gmail.com>
2022-12-04 23:29:26 +0100

    Prepared day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+
+string assemblyPath = Assembly.GetExecutingAssembly().Location;
+string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+string textPath = Path.Combine(assemblyDirectory, "input.txt");
+
+string[] input = File.ReadAllLines(textPath);
+
+// Part 1
+
+int counter = 0;
+
+foreach (string line in input)
+{
+}
+
+Console.WriteLine($"Teil 1: {counter}");
