79bebd881720735343f2ff4feaaf0751ad048a31
Jürgen Sunkler <juergen.sunkler@gmail.com>
2022-12-06 19:25:55 +0100

    Added debug-options to old projects.

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -3,7 +3,11 @@ using System.Text.RegularExpressions;
 
 string assemblyPath = Assembly.GetExecutingAssembly().Location;
 string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+#if DEBUG
+string textPath = Path.Combine(assemblyDirectory, "input.debug.txt");
+#else
 string textPath = Path.Combine(assemblyDirectory, "input.txt");
+#endif
 
 string[] input = File.ReadAllLines(textPath);
 
