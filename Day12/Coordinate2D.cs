ba0f6c221787827137740271cac394c82350aac4
j.sunkler <j.sunkler@orgacard.de>
2022-12-12 09:29:38 +0100

    finished day 12

---
+++ Day12/Coordinate2D.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    internal class Coordinate2D
+    {
+        public static readonly Coordinate2D origin = new(0, 0);
+        public static readonly Coordinate2D unit_x = new(1, 0);
+        public static readonly Coordinate2D unit_y = new(0, 1);
+        public readonly int x;
+        public readonly int y;
+
+        public Coordinate2D(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Coordinate2D((int x, int y) coord)
+        {
+            this.x = coord.x;
+            this.y = coord.y;
+        }
+
+        public Coordinate2D RotateCW(int degrees, Coordinate2D center)
+        {
+            Coordinate2D offset = center - this;
+            return center + offset.RotateCW(degrees);
+        }
+        public Coordinate2D RotateCW(int degrees)
+        {
+            return ((degrees / 90) % 4) switch
+            {
+                0 => this,
+                1 => RotateCW(),
+                2 => -this,
+                3 => RotateCCW(),
+                _ => this,
+            };
+        }
+
+        private Coordinate2D RotateCW()
+        {
+            return new Coordinate2D(y, -x);
+        }
+
+        public Coordinate2D RotateCCW(int degrees, Coordinate2D center)
+        {
+            Coordinate2D offset = center - this;
+            return center + offset.RotateCCW(degrees);
+        }
+        public Coordinate2D RotateCCW(int degrees)
+        {
+            return ((degrees / 90) % 4) switch
+            {
+                0 => this,
+                1 => RotateCCW(),
+                2 => -this,
+                3 => RotateCW(),
+                _ => this,
+            };
+        }
+
+        private Coordinate2D RotateCCW()
+        {
+            return new Coordinate2D(-y, x);
+        }
+
+        public static Coordinate2D operator +(Coordinate2D a) => a;
+        public static Coordinate2D operator +(Coordinate2D a, Coordinate2D b) => new(a.x + b.x, a.y + b.y);
+        public static Coordinate2D operator -(Coordinate2D a) => new(-a.x, -a.y);
+        public static Coordinate2D operator -(Coordinate2D a, Coordinate2D b) => a + (-b);
+        public static Coordinate2D operator *(int scale, Coordinate2D a) => new(scale * a.x, scale * a.y);
+        public static bool operator ==(Coordinate2D a, Coordinate2D b) => (a.x == b.x && a.y == b.y);
+        public static bool operator !=(Coordinate2D a, Coordinate2D b) => (a.x != b.x || a.y != b.y);
+
+        public static implicit operator Coordinate2D((int x, int y) a) => new(a.x, a.y);
+
+        public static implicit operator (int x, int y)(Coordinate2D a) => (a.x, a.y);
+
+        public int ManDistance(Coordinate2D other)
+        {
+            int x = Math.Abs(this.x - other.x);
+            int y = Math.Abs(this.y - other.y);
+            return x + y;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != typeof(Coordinate2D)) return false;
+            return this == (Coordinate2D)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return (100 * x + y).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Concat("(", x, ", ", y, ")");
+        }
+        public void Deconstruct(out int xVal, out int yVal)
+        {
+            xVal = x;
+            yVal = y;
+        }
+    }
+
+    internal static class Extenstions
+    {
+        public static List<Coordinate2D> Neighbors(this Coordinate2D val, bool includeDiagonals = false)
+        {
+            var tmp = new List<Coordinate2D>()
+                {
+                    new Coordinate2D(val.x - 1, val.y),
+                    new Coordinate2D(val.x + 1, val.y),
+                    new Coordinate2D(val.x, val.y - 1),
+                    new Coordinate2D(val.x, val.y + 1),
+                };
+            if (includeDiagonals)
+            {
+                tmp.AddRange(new List<Coordinate2D>()
+                    {
+                        new Coordinate2D(val.x - 1, val.y - 1),
+                        new Coordinate2D(val.x + 1, val.y - 1),
+                        new Coordinate2D(val.x - 1, val.y + 1),
+                        new Coordinate2D(val.x + 1, val.y + 1),
+                    });
+            }
+            return tmp;
+        }
+    }
+
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Reflection;
+
+namespace Day12
+{
+    internal class Program
+    {
+        static readonly Dictionary<Coordinate2D, int> Map = new();
+        static Coordinate2D start;
+        static Coordinate2D end;
+
+        static void Main(string[] args)
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+#if DEBUG
+            string textPath = Path.Combine(assemblyDirectory, "input.debug.txt");
+#else
+            string textPath = Path.Combine(assemblyDirectory, "input.txt");
+#endif
+
+            string[] input = File.ReadAllLines(textPath);
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    if (input[y][x] == 'S')
+                    {
+                        start = (x, y);
+                        Map[start] = 1;
+                    }
+                    else if (input[y][x] == 'E')
+                    {
+                        end = (x, y);
+                        Map[end] = 26;
+                    }
+                    else
+                    {
+                        Map[(x, y)] = input[y][x] - 96;
+                    }
+                }
+            }
+
+            // Part 1
+
+            Console.WriteLine($"Teil 1: {AStar(Map, start, end).Count - 1}");
+
+            // Part 2
+
+            List<int> lengths = new List<int>();
+
+            List<KeyValuePair<Coordinate2D, int>> low = Map.Where(p => p.Value == 1).ToList();
+
+            foreach (var kvp in low)
+            {
+                var path = AStar(Map, kvp.Key, end);
+                if (path[0] == kvp.Key) lengths.Add(path.Count - 1);
+            }
+
+            Console.WriteLine($"Teil 2: {lengths.Min()}");
+        }
+
+        static List<Coordinate2D> AStar(Dictionary<Coordinate2D, int> Map, Coordinate2D start, Coordinate2D end)
+        {
+            Dictionary<Coordinate2D, Coordinate2D> cameFrom = new();
+            PriorityQueue<Coordinate2D, int> openSet = new();
+            Dictionary<Coordinate2D, int> gScore = new();
+            Dictionary<Coordinate2D, int> fScore = new();
+            HashSet<Coordinate2D> closedSet = new();
+
+            gScore[start] = 0;
+            fScore[start] = start.ManDistance(end);
+            openSet.Enqueue(start, 0);
+
+            while (openSet.Count > 0)
+            {
+                Coordinate2D current = openSet.Dequeue();
+                if (!closedSet.Add(current)) continue;
+                if (current == end) break;
+                foreach (var n in current.Neighbors())
+                {
+                    if (Map.TryGetValue(n, out int height) && height <= Map[current] + 1)
+                    {
+                        var tGscore = gScore[current] + 1;
+                        if (tGscore < gScore.GetValueOrDefault(n, int.MaxValue))
+                        {
+                            cameFrom[n] = current;
+                            gScore[n] = tGscore;
+                            fScore[n] = tGscore + n.ManDistance(end);
+                            openSet.Enqueue(n, tGscore + n.ManDistance(end));
+                        }
+                    }
+                }
+            }
+
+            return ReconstructPath(cameFrom, end);
+        }
+
+        static List<Coordinate2D> ReconstructPath(Dictionary<Coordinate2D, Coordinate2D> cameFrom, Coordinate2D current)
+        {
+            List<Coordinate2D> path = new() { current };
+            while (cameFrom.TryGetValue(current, out var next))
+            {
+                path.Add(next);
+                current = next;
+            }
+            path.Reverse();
+            return path;
+        }
+    }
+}
