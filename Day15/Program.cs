f2d7f1084e53f0244764c7a81e2cbf8d9bd00e94
j.sunkler <j.sunkler@orgacard.de>
2022-12-15 08:41:27 +0100

    finished day 15 part 1

---
+++ Day15/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Day15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            string assemblyDirectory = Path.GetDirectoryName(assemblyPath);
+#if DEBUG
+            string textPath = Path.Combine(assemblyDirectory, "input.debug.txt");
+#else
+            string textPath = Path.Combine(assemblyDirectory, "input.txt");
+#endif
+
+            string[] input = File.ReadAllLines(textPath);
+
+            int counter = 0;
+
+            Regex regex = new Regex(@"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$", RegexOptions.Compiled);
+            List<Sensor> sensors = new List<Sensor>();
+
+            int minX = 0;
+            int minY = 0;
+            int maxX = 0;
+            int maxY = 0;
+
+            foreach (string line in input)
+            {
+                var match = regex.Match(line);
+                int sx = int.Parse(match.Groups[1].Value);
+                int sy = int.Parse(match.Groups[2].Value);
+                int bx = int.Parse(match.Groups[3].Value);
+                int by = int.Parse(match.Groups[4].Value);
+                Sensor temp = new Sensor((sx, sy), (bx, by));
+
+                minX = Math.Min(minX, sx - temp.CoveredDistance);
+                minY = Math.Min(minY, sy - temp.CoveredDistance);
+                maxX = Math.Max(maxX, sx + temp.CoveredDistance);
+                maxY = Math.Max(maxY, sy + temp.CoveredDistance);
+
+                sensors.Add(temp);
+            }
+
+            // Part 1
+
+#if DEBUG
+            int Y = 10;
+#else
+            int Y = 2_000_000;
+#endif
+
+            for (int x = minX; x <= maxX; x++)
+            {
+                bool covered = false;
+
+                if (sensors.Where(s => s.SensorPosition == (x, Y) || s.BeaconPosition == (x, Y)).Any()) continue;
+
+                foreach (Sensor sensor in sensors) 
+                { 
+                    if (sensor.CoversPoint((x, Y)))
+                    {
+                        covered = true;
+                        break;
+                    }
+                }
+
+                if (covered) counter++;
+            }
+
+            Console.WriteLine($"Teil 1: {counter}");
+
+            // Part 2
+
+#if DEBUG
+            int searchSpace = 20;
+#else
+            int searchSpace = 4_000_000;
+#endif
+
+            for (int x = 0; x < searchSpace; x++)
+            {
+                for (int y = 0; y < searchSpace; y++)
+                {
+                    bool covered = false;
+
+                    if (sensors.Where(s => s.SensorPosition == (x, y) || s.BeaconPosition == (x, y)).Any()) continue;
+
+                    foreach (Sensor sensor in sensors)
+                    {
+                        if (sensor.CoversPoint((x, y)))
+                        {
+                            covered = true;
+                            break;
+                        }
+                    }
+
+                    if (!covered)
+                    {
+                        Console.WriteLine($"Teil 2: {x * 4_000_000 + y}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Day15/Sensor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    internal class Sensor
+    {
+        public (int x, int y) SensorPosition { get; set; }
+
+        public (int x, int y) BeaconPosition { get; set; }
+
+        public int CoveredDistance
+        {
+            get
+            {
+                return Math.Abs(SensorPosition.x - BeaconPosition.x) + Math.Abs(SensorPosition.y - BeaconPosition.y);
+            }
+        }
+
+        public bool CoversPoint((int x, int y) point)
+        {
+            int d = Math.Abs(SensorPosition.x - point.x) + Math.Abs(SensorPosition.y - point.y);
+            return d <= CoveredDistance;
+        }
+
+        public HashSet<(int x, int y)> CoveredPoints
+        {
+            get
+            {
+                HashSet<(int x, int y)> temp = new()
+                {
+                    (SensorPosition.x, SensorPosition.y),
+                    (BeaconPosition.x, BeaconPosition.y)
+                };
+
+                for (int x = SensorPosition.x - CoveredDistance; x <= SensorPosition.x + CoveredDistance; x++)
+                {
+                    for (int y = SensorPosition.y - CoveredDistance; y <= SensorPosition.y + CoveredDistance; y++)
+                    {
+                        if (CoversPoint((x, y))) temp.Add((x, y));
+                    }
+                }
+
+                return temp;
+            }
+        }
+
+        public Sensor(
+            (int, int) sensor,
+            (int, int) beacon
+            )
+        {
+            SensorPosition = sensor;
+            BeaconPosition = beacon;
+        }
+
+    }
+}
